43bd25c249dbee4faf8c46d847eee53749a00ca2
Ali Bahari <alibahari007@gmail.com>
2022-08-24 10:11:32 +0430

    Add a sample function

--- cards/main.go
+++ cards/main.go
@@ -2,13 +2,19 @@ package main
 
 import "fmt"
 
+func newString() string {
+	return "Card 4"
+}
+
 func main() {
 	// Different ways to declare a variable
 	var card_1 string = "Card 1"
 	var card_2 = "Card 2"
 	card_3 := "Card 3"
+	card_4 := newString()
 
 	fmt.Println(card_1)
 	fmt.Println(card_2)
 	fmt.Println(card_3)
+	fmt.Println(card_4)
 }
