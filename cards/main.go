2626ce51eee2ba135947ff226c4b060b8567e3a9
Ali Bahari <alibahari007@gmail.com>
2022-08-24 10:01:45 +0430

    Add new project folder plus code

---
+++ cards/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Different ways to declare a variable
+	var card_1 string = "Card 1"
+	var card_2 = "Card 2"
+	card_3 := "Card 3"
+
+	fmt.Println(card_1)
+	fmt.Println(card_2)
+	fmt.Println(card_3)
+}
