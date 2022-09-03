8667b10f72471f229b0a59e93d1a7b4c4d852271
Ali Bahari <alibahari007@gmail.com>
2022-09-03 19:33:55 +0430

    Add correct code for interfaces

--- interfaces/main.go
+++ interfaces/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+// This means that any type that has a func of getGreeting() which returns a string is also a bot
+// We can also provide the arguments that will be the input of the func in the ()
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
@@ -23,13 +29,8 @@ func (spanishBot) getGreeting() string {
 	return "Hola Hector!"
 }
 
-// SIMILAR logic in both languages
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
-}
-
-func printGreeting(sb spanishBot) {
-	fmt.Println(sb.getGreeting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
 // In Go funcs with the same name but different arguments are not permitted
